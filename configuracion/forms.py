4fcc3f71ecb9cb76c10374031ec36a229d2f2f87
juandiegoimbachihoyos <juandiegoimbachihoyos@gmail.com>
2025-05-21 18:24:53 -0500

    Correccines de flujo / App de clientes 2 /90%

--- configuracion/forms.py
+++ configuracion/forms.py
@@ -6,7 +6,29 @@ class EspecieForm(forms.ModelForm):
         model = Especie
         fields = ['nombre', 'descripcion']
         widgets = {
-            'descripcion': forms.Textarea(attrs={'rows': 3}),
+            'nombre': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el nombre de la especie'
+            }),
+            'descripcion': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Descripción opcional de la especie'
+            }),
+        }
+        labels = {
+            'nombre': 'Nombre de la especie',
+            'descripcion': 'Descripción',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Hacer que el campo nombre sea requerido
+        self.fields['nombre'].required = True
+        # Personalizar mensajes de error
+        self.fields['nombre'].error_messages = {
+            'required': 'El nombre de la especie es obligatorio.',
+            'unique': 'Ya existe una especie con este nombre.',
         }
 
 class RazaForm(forms.ModelForm):
@@ -14,5 +36,42 @@ class RazaForm(forms.ModelForm):
         model = Raza
         fields = ['nombre', 'especie', 'descripcion']
         widgets = {
-            'descripcion': forms.Textarea(attrs={'rows': 3}),
+            'nombre': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el nombre de la raza'
+            }),
+            'especie': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'descripcion': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Descripción opcional de la raza'
+            }),
         }
+        labels = {
+            'nombre': 'Nombre de la raza',
+            'especie': 'Especie',
+            'descripcion': 'Descripción',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Hacer que los campos sean requeridos
+        self.fields['nombre'].required = True
+        self.fields['especie'].required = True
+        
+        # Personalizar mensajes de error
+        self.fields['nombre'].error_messages = {
+            'required': 'El nombre de la raza es obligatorio.',
+        }
+        self.fields['especie'].error_messages = {
+            'required': 'Debe seleccionar una especie.',
+        }
+        
+        # Mejorar el select de especies
+        self.fields['especie'].empty_label = "-- Seleccione una especie --"
+        
+        # Verificar si hay especies disponibles
+        if not self.fields['especie'].queryset.exists():
+            self.fields['especie'].widget.attrs['disabled'] = True
--- configuracion/urls.py
+++ configuracion/urls.py
@@ -6,13 +6,13 @@ app_name = 'configuracion'
 urlpatterns = [
     # URLs para Especies
     path('especies/', views.EspecieListView.as_view(), name='lista_especies'),
-    path('especies/crear/', views.EspecieCreateView.as_view(), name='crear_especie'), #Falla
-    path('especie/<int:pk>/editar/', views.EspecieUpdateView.as_view(), name='editar_especie'), #Falla
+    path('especies/crear/', views.EspecieCreateView.as_view(), name='crear_especie'), 
+    path('especie/<int:pk>/editar/', views.EspecieUpdateView.as_view(), name='editar_especie'), 
     path('especie/<int:pk>/eliminar/', views.EspecieDeleteView.as_view(), name='eliminar_especie'),
     
     # URLs para Razas
     path('razas/', views.RazaListView.as_view(), name='lista_razas'),
-    path('razas/crear/', views.RazaCreateView.as_view(), name='crear_raza'), # Falla
-    path('raza/<int:pk>/editar/', views.RazaUpdateView.as_view(), name='editar_raza'), #Falla 
+    path('razas/crear/', views.RazaCreateView.as_view(), name='crear_raza'),
+    path('raza/<int:pk>/editar/', views.RazaUpdateView.as_view(), name='editar_raza'), 
     path('raza/<int:pk>/eliminar/', views.RazaDeleteView.as_view(), name='eliminar_raza'),
 ]
