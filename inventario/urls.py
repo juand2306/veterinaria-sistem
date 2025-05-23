7f1f283ebcba83c73e431303d8c86fc734ddd355
juandiegoimbachihoyos <juandiegoimbachihoyos@gmail.com>
2025-05-23 18:25:03 -0500

    Arreglos en la app de inventario / 99%

--- inventario/urls.py
+++ inventario/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('vacuna/<int:pk>/editar/', views.VacunaUpdateView.as_view(), name='editar_vacuna'), 
     path('vacuna/<int:pk>/eliminar/', views.VacunaDeleteView.as_view(), name='eliminar_vacuna'),
     
-    # URLs para Vacunas Aplicadas #Desconectada
+    # URLs para Vacunas Aplicadas
     path('mascota/<int:mascota_id>/vacuna/aplicar/', views.VacunaAplicadaCreateView.as_view(), name='aplicar_vacuna'),
     path('vacuna-aplicada/<int:pk>/', views.VacunaAplicadaDetailView.as_view(), name='detalle_vacuna_aplicada'),
     path('vacuna-aplicada/<int:pk>/editar/', views.VacunaAplicadaUpdateView.as_view(), name='editar_vacuna_aplicada'),
@@ -25,7 +25,7 @@ urlpatterns = [
     path('producto/<int:pk>/editar/', views.ProductoUpdateView.as_view(), name='editar_producto'),
     path('producto/<int:pk>/eliminar/', views.ProductoDeleteView.as_view(), name='eliminar_producto'),
     
-    # URLs para Productos Aplicados #Desconectada
+    # URLs para Productos Aplicados
     path('mascota/<int:mascota_id>/producto/aplicar/', views.ProductoAplicadoCreateView.as_view(), name='aplicar_producto'),
     path('producto-aplicado/<int:pk>/', views.ProductoAplicadoDetailView.as_view(), name='detalle_producto_aplicado'),
     path('producto-aplicado/<int:pk>/editar/', views.ProductoAplicadoUpdateView.as_view(), name='editar_producto_aplicado'),
